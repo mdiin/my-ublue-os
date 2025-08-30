7988f4c61ddc014080eb37d85227f48363c00cc0
Matthias Varberg Ingesman <mvi@ingesman.dk>
2025-08-30 11:08:00 +0200

    feat: switch to Bazzite Nvidia base image
    This prepares it for use on my desktop.

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,7 @@ set -ouex pipefail
 # this installs a package from fedora repos
 # dnf5 remove -y homebrew
 
-dnf5 -y remove plasma-workspace plasma-* kde-*
+# dnf5 -y remove plasma-workspace plasma-* kde-*
 
 # setup hyprland from COPR
 dnf5 -y copr enable solopasha/hyprland
