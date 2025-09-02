734c2e635e0d1c396206a5e654433cdae7f37ebb
Matthias Varberg Ingesman <mvi@ingesman.dk>
2025-09-02 20:31:27 +0200

    fix: remove comments inside install

--- build_files/build.sh
+++ build_files/build.sh
@@ -36,10 +36,6 @@ dnf5 -y copr disable solopasha/hyprland
 # more desktop-environment utils
 dnf5 -y install \
         pipewire \
-        # sddm \
-        # sddm-wayland-generic \
-        # sddm-conf \
-        # sddm-breeze \
         brightnessctl \
         flameshot \
         fuzzel \
